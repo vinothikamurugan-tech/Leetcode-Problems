955eb7f4fa528a7c1c363364bbd22dea4a5bd399
vinothikamurugan-tech <vinothikamurugan@gmail.com>
2025-11-28 21:11:59 +0530

    Add files via upload

---
+++ leetcode 9.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        int rev=0;
+        int org = x;
+        while(x>0){
+            int temp = x%10;
+            rev = rev*10+temp;
+            x=x/10;
+        }
+        if(rev==org){
+            return true;
+        }
+        return false;
+    }
+
+}
