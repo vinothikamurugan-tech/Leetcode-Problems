61bafedbd394ae5d892f4047807b67ec8219f1fb
vinothikamurugan-tech <vinothikamurugan@gmail.com>
2025-12-03 23:13:06 +0530

    Add files via upload

---
+++ leetcode 71.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String simplifyPath(String path) {
+        String[] parts = path.split("/");
+        Stack<String> stack = new Stack<>();
+
+        for (String part : parts) {
+            if (part.equals("") || part.equals(".")) {
+                continue;
+            }
+            if (part.equals("..")) {
+                if (!stack.isEmpty()) stack.pop();
+            } else {
+                stack.push(part);
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (String dir : stack) {
+            result.append("/").append(dir);
+        }
+
+        return result.length() > 0 ? result.toString() : "/";
+    }
+}
