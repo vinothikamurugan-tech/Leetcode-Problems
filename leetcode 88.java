78bda623aeba2c7334e014f26282942bad6da518
Vinothika Murugan <vinothikamurugan@gmail.com>
2026-07-08 14:37:55 +0530

    Update leetcode 88.java

--- leetcode 88.java
+++ leetcode 88.java
@@ -4,7 +4,7 @@ class Solution {
         int j = n - 1;
         int k = m + n - 1;
 
-        while (j >= 0) {
+        while (j >= 0) { 
             if (i >= 0 && nums1[i] > nums2[j]) {
                 nums1[k] = nums1[i];
                 i--;
